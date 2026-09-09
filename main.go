6f1eb30a11b54db7793af2848d90be8a6c40e077
shkn1-marko <shkn1.marko@gmail.com>
2026-09-09 13:26:52 +0200

    feat: add gdep make command

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const baseDir = "/etc/gdep"
 
 func main() {
-	fmt.Println("gdep test run")
+	if len(os.Args) < 3 {
+		fmt.Println("usage: gdep <command> <name>")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "make":
+		makeProject(os.Args[2])
+	default:
+		fmt.Println("unknown command:", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+func makeProject(name string) {
+	dir := filepath.Join(baseDir, name)
+
+	if _, err := os.Stat(dir); err == nil {
+		fmt.Printf("project %q already exists\n", name)
+		os.Exit(1)
+	}
+
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	for _, script := range []string{"build.sh", "deploy.sh"} {
+		path := filepath.Join(dir, script)
+		content := "#!/bin/bash\n# exit 0 on success, nonzero on failure\n"
+		if err := os.WriteFile(path, []byte(content), 0o755); err != nil {
+			fmt.Println("error:", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("created %s\n", dir)
 }
