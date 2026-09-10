cd4836ede9536555a792f100fd4dfc55e09fccf2
shkn1-marko <shkn1.marko@gmail.com>
2026-09-10 07:52:14 +0200

    add gdep remove and list commands

--- main.go
+++ main.go
@@ -9,16 +9,29 @@ import (
 const baseDir = "/etc/gdep"
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("usage: gdep <command> <name>")
+	if len(os.Args) < 2 {
+		fmt.Println("usage: gdep <command> [name]")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "make":
+		requireName()
 		makeProject(os.Args[2])
+	case "remove":
+		requireName()
+		removeProject(os.Args[2])
+	case "list":
+		listProjects()
 	default:
 		fmt.Println("unknown command:", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+func requireName() {
+	if len(os.Args) < 3 {
+		fmt.Println("usage: gdep", os.Args[1], "<name>")
 		os.Exit(1)
 	}
 }
@@ -46,4 +59,39 @@ func makeProject(name string) {
 	}
 
 	fmt.Printf("created %s\n", dir)
+}
+
+func removeProject(name string) {
+	dir := filepath.Join(baseDir, name)
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Printf("project %q does not exist\n", name)
+		os.Exit(1)
+	}
+
+	if err := os.RemoveAll(dir); err != nil {
+		fmt.Println("error", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("removed %s\n", dir)
+}
+
+func listProjects() {
+	entries, err := os.ReadDir(baseDir)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	if len(entries) == 0 {
+		fmt.Println("no projects registered")
+		return
+	}
+
+	for _, e := range entries {
+		if e.IsDir() {
+			fmt.Println(e.Name())
+		}
+	}
 }
