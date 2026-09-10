0f83bc0f625374d9a356db1f6b458f1c39008262
shkn1-marko <shkn1.marko@gmail.com>
2026-09-10 08:37:17 +0200

    refactor: split gdep output from project logic

--- main.go
+++ main.go
@@ -3,95 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 )
-
-const baseDir = "/etc/gdep"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: gdep <command> [name]")
-		os.Exit(1)
+		fail(fmt.Errorf("usage: gdep <command> [name]"))
 	}
 
 	switch os.Args[1] {
 	case "make":
 		requireName()
-		makeProject(os.Args[2])
+		if err := Make(os.Args[2]); err != nil {
+			fail(err)
+		}
+		fmt.Printf("created /etc/gdep/%s\n", os.Args[2])
+
 	case "remove":
 		requireName()
-		removeProject(os.Args[2])
+		if err := Remove(os.Args[2]); err != nil {
+			fail(err)
+		}
+		fmt.Printf("removed /etc/gdep/%s\n", os.Args[2])
+
 	case "list":
-		listProjects()
+		names, err := List()
+		if err != nil {
+			fail(err)
+		}
+		if len(names) == 0 {
+			fmt.Println("no projects registered")
+			return
+		}
+		for _, n := range names {
+			fmt.Println(n)
+		}
+
 	default:
-		fmt.Println("unknown command:", os.Args[1])
-		os.Exit(1)
+		fail(fmt.Errorf("unknown command: %s", os.Args[1]))
 	}
 }
 
 func requireName() {
 	if len(os.Args) < 3 {
-		fmt.Println("usage: gdep", os.Args[1], "<name>")
-		os.Exit(1)
+		fail(fmt.Errorf("usage: gdep %s <name>", os.Args[1]))
 	}
 }
 
-func makeProject(name string) {
-	dir := filepath.Join(baseDir, name)
-
-	if _, err := os.Stat(dir); err == nil {
-		fmt.Printf("project %q already exists\n", name)
-		os.Exit(1)
-	}
-
-	if err := os.MkdirAll(dir, 0o755); err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-
-	for _, script := range []string{"build.sh", "deploy.sh"} {
-		path := filepath.Join(dir, script)
-		content := "#!/bin/bash\n# exit 0 on success, nonzero on failure\n"
-		if err := os.WriteFile(path, []byte(content), 0o755); err != nil {
-			fmt.Println("error:", err)
-			os.Exit(1)
-		}
-	}
-
-	fmt.Printf("created %s\n", dir)
-}
-
-func removeProject(name string) {
-	dir := filepath.Join(baseDir, name)
-
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		fmt.Printf("project %q does not exist\n", name)
-		os.Exit(1)
-	}
-
-	if err := os.RemoveAll(dir); err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("removed %s\n", dir)
-}
-
-func listProjects() {
-	entries, err := os.ReadDir(baseDir)
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-
-	if len(entries) == 0 {
-		fmt.Println("no projects registered")
-		return
-	}
-
-	for _, e := range entries {
-		if e.IsDir() {
-			fmt.Println(e.Name())
-		}
-	}
+func fail(err error) {
+	fmt.Fprintln(os.Stderr, "error:", err)
+	os.Exit(1)
 }
